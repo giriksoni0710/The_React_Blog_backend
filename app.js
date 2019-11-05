bcb84c3e007e774f962269e169db4a412466eeb3
Girik soni <giriksoni0710@gmail.com>
2019-11-05 13:48:48 -0800

    complete backend with nodejs and mongodb

--- app.js
+++ app.js
@@ -3,70 +3,107 @@ const express = require('express');
 
 const app = express();
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb')
+
 
 app.use(bodyParser.json());
 
+// common db information function
 
-// here we store the upvotes and comment data of the article
+const withdb =  async (operations) =>{
 
-var articlesinfo = {
-
-    'learn-react': {
-      upvotes:  0,
-      comments: []
-    },
-    'learn-node': {
-        upvotes:  0,
-        comments: []
-   
-    },
-    'my-thoughts-on-resumes': {
-        upvotes:  0,
-        comments: []
+    try{
+       
+        const connection = await mongodb.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+        
+        const db = connection.db('my-blog');
+    
+        await operations(db);
+       
+        connection.close()
         }
+        catch(err){
+            console.log(err)
+            res.send("there is an error ",err)
+        }
+
 
 }
 
-//increase upvote everytime the request is made
 
-app.post('/articles/:name/upvote', (req, res)=>{
+//getting article details
 
+app.get('/articles/:name',async (req, res)=>{
 
-    const article_name = req.params.name;
-
-    articlesinfo[article_name].upvotes += 1; 
-
-    res.status(200).send(`the article has ${articlesinfo[article_name].upvotes} likes`);
+    withdb(async (db, res)=>{
 
     
+    const article_name = req.params.name;
 
+    const articlesinfo = await db.collection('articles').findOne({});
+        
+    res.status(200).send(articlesinfo);
+
+    });
 })
 
-//adding a comment from the comment 
-app.post('/articles/:name/addcomment', (req, res)=>{
+//increase upvote everytime the request is made
+
+app.post('/articles/:name/upvote',async (req, res)=>{
 
 
+    withdb(async (db)=>{
     const article_name = req.params.name;
 
-    const username = req.body.username;
-    const email = req.body.email;
-    const comment = req.body.comment;
+    const article_upvotes = await db.collection('articles').findOne({ name: article_name }); 
+                            await db.collection('articles').updateOne({name: article_name},
+       { '$set': {
 
-    
-    
-    articlesinfo[article_name].comments.push({username, comment, email});
+        name: article_name,
+        upvotes: article_upvotes.upvotes+1
+        
+        }
+    });
 
-    console.log(articlesinfo[article_name].comments[0]);
-
-    if(articlesinfo[article_name].comments!=null){
+    const updated_articleinfo = await db.collection('articles').findOne({ name: article_name })
     
-        res.send(articlesinfo[article_name]);
+    res.send(updated_articleinfo);
 
-    }
+    },res);
 
+
+})
+//adding a comment from the comment 
+app.post('/articles/:name/addcomment', async (req, res)=>{
+
+  
+    withdb(async (db)=>{
+        const username = req.body.username;
+        const email = req.body.email;
+        const comment = req.body.comment;
     
-   
+        const article_name = req.params.name;
     
+        const article_upvotes = await db.collection('articles').findOne({ name: article_name }); 
+                                await db.collection('articles').updateOne({name: article_name},
+           { '$set': {
+    
+            name: article_name,
+            comments: article_upvotes.comments.concat(
+                {
+                    name: username,
+                    comment: comment,
+                    email: email
+                })
+            
+            
+            }
+        },res);
+    
+        const updated_articleinfo = await db.collection('articles').findOne({ name: article_name })
+        
+        res.status(200).send(updated_articleinfo);
+    })
 
 })
 
